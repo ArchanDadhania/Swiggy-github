9b46a07a568006963aa77346cdf4b0f3190b93a0
Shiva <archandadhaniya@yahoo.com>
2024-06-08 01:00:16 +0530

    change in the main java.

--- Application/src/Main.java
+++ Application/src/Main.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        Player playerA = new Player(50,5,10);
-        Player playerB = new Player(100,10,5);
+        Scanner scanner = new Scanner(System.in);
+        // Get Player A attributes
+        System.out.println("Enter Player A attributes:");
+        System.out.print("Health: ");
+        int playerAHealth = scanner.nextInt();
+        System.out.print("Strength: ");
+        int playerAStrength = scanner.nextInt();
+        System.out.print("Attack: ");
+        int playerAAttack = scanner.nextInt();
 
-        Arena arena = new Arena(playerA,playerB);
+
+        // Get Player B attributes
+        System.out.println("Enter Player B attributes:");
+        System.out.print("Health: ");
+        int playerBHealth = scanner.nextInt();
+        System.out.print("Strength: ");
+        int playerBStrength = scanner.nextInt();
+        System.out.print("Attack: ");
+        int playerBAttack = scanner.nextInt();
+
+
+        // Create players
+        Player playerA = new Player(playerAHealth, playerAStrength, playerAAttack);
+        Player playerB = new Player(playerBHealth, playerBStrength, playerBAttack);
+
+
+        // Create arena and start the fight
+        Arena arena = new Arena(playerA, playerB);
         arena.fight();
     }
 }
